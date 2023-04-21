7cf4ef25ba41e8a3de1a2cbc33cc9e91ff52c792
Margaret-hub <dwamenamagaret91@gmail.com>
2023-04-20 20:36:42 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * main - serves as the starting point for program execution.
  *
- * Return: Always 0 (Success)*/
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("with proper grammar, but the outcome is a piece of art,"\n);
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
