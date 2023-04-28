627dea689abb68e26e187dbaa807d13943688f85
Margaret-hub <dwamenamagaret91@gmail.com>
2023-04-27 21:12:34 -0700

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* more_numbers - nmbr fct
+* Return: no return
+*/
+
+void more_numbers(void)
+{
+
+int i, j;
+for (i = 1; i <= 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j >= 10)
+putchar('1');
+putchar(j % 10 + '0');
+}
+putchar('\n');
+}
+}
