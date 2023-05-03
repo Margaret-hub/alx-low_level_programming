c9234ddc991af408149762a26a020ff619764f78
Margaret-hub <dwamenamagaret91@gmail.com>
2023-05-02 17:18:42 -0700

    I do not fear computers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: the string
+ *
+ * Return: the length of the string
+ */
+void _puts(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+putchar(str[i]);
+i++;
+}
+putchar('\n');
+}
