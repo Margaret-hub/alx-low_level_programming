a1398673deeb858955114a79f4b82dc1c36fdf6c
Margaret-hub <dwamenamagaret91@gmail.com>
2023-08-21 12:20:11 -0700

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,24 @@
+/**
+ * basic c operations (arithmetics)
+ * hans254
+ */
+int add(int c, int d)
+{
+    return (c + d);
+}
+int sub(int c, int d)
+{
+    return (c - d);
+}
+int mul(int c, int d)
+{
+    return (c * d);
+}
+int div(int c, int d)
+{
+    return (c / d);
+}
+int mod(int c, int d)
+{
+    return (c % d);
+}
